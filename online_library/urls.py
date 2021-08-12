9ca08aae9701ef9f43e30a4feb7dbc5e62708f0c
svaradinov <sv.varadinov@gmail.com>
2021-08-13 00:24:41 +0300

    Add apps

---
+++ online_library/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('book', include('online_library.book.urls')),
+    path('user', include('online_library.user.urls')),
+]
